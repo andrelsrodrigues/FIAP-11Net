959a9a628fee04c9dcb4ddab939782e532d30cdd
André Rodrigues <andrelsrodrigues@gmail.com>
2017-09-10 21:27:19 -0300

    Inclusão da classe de Contexto para conexão e definição do modelo de dados.

---
+++ BoardGamesMVC/BoardGamesMVC/Data/BoardGamesContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using BoardGamesMVC.Models;
+
+namespace BoardGamesMVC.Data
+{
+    public class BoardGamesContext : DbContext
+    {
+        public BoardGamesContext() 
+            : base(new DbContextOptionsBuilder<BoardGamesContext>()
+                                    .UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=DBBoardGames;Trusted_Connection=True;MultipleActiveResultSets=true").Options){ }
+
+
+        public DbSet<BoardGame> BoardGames { get; set; }
+        public DbSet<Partida> Partidas { get; set; }
+        public DbSet<JogadorPartida> JogadoresPartidas { get; set; }
+        public DbSet<Jogador> Jogadores { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BoardGame>().ToTable("TBBoardGames");
+            modelBuilder.Entity<Partida>().ToTable("TBPartidas");
+            modelBuilder.Entity<JogadorPartida>().ToTable("TBJogadoresPartidas");
+            modelBuilder.Entity<Jogador>().ToTable("TBJogadores");
+        }
+
+    }
+}
